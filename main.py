e8efaed8cad6e569e1131ddaa383257466fa834e
Aadi Patangi <87159377+AadiPatangi@users.noreply.github.com>
2021-12-12 04:11:25 +0000

    added images of the companies so its easier for the user to know what data he is looking at

--- main.py
+++ main.py
@@ -1,13 +1,25 @@
 import yfinance as yf
-
+import climage
 
 
 #Apple Holdings
-apple = yf.download('AAPL', period = '1d')
+AppleIMG = climage.convert('apple.png') #read the image
+print(AppleIMG) 
 
+apple = yf.download('AAPL', period = '1d')
 print(apple)
 print("******************************")
-#DowJones
-DowJones = yf.download('QQQM', period = '1d')
+#DowJones Holdings
+DJIMG = climage.convert('dowjones.png') 
+print(DJIMG) 
 
+DowJones = yf.download('QQQM', period = '1d')
 print(DowJones)
+print("******************************")
+
+#BMW Holdings
+BMWIMG = climage.convert('bmw.png') #read the image
+print(BMWIMG) 
+
+bmw = yf.download('BMWYY', period = '1d')
+print(bmw)
