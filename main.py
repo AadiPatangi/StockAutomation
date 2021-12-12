5186bc821f937dd6fe9acd96b4f8ef71d09f168b
Aadi Patangi <87159377+AadiPatangi@users.noreply.github.com>
2021-12-12 04:22:42 +0000

    Changed the width of the image so it does not fill the console

--- main.py
+++ main.py
@@ -3,14 +3,14 @@ import climage
 
 
 #Apple Holdings
-AppleIMG = climage.convert('apple.png') #read the image
+AppleIMG = climage.convert('apple.png',width = 40) #read the image
 print(AppleIMG) 
 
 apple = yf.download('AAPL', period = '1d')
 print(apple)
 print("******************************")
 #DowJones Holdings
-DJIMG = climage.convert('dowjones.png') 
+DJIMG = climage.convert('dowjones.png',width = 40) 
 print(DJIMG) 
 
 DowJones = yf.download('QQQM', period = '1d')
@@ -18,7 +18,7 @@ print(DowJones)
 print("******************************")
 
 #BMW Holdings
-BMWIMG = climage.convert('bmw.png') #read the image
+BMWIMG = climage.convert('bmw.png', width = 40) #read the image
 print(BMWIMG) 
 
 bmw = yf.download('BMWYY', period = '1d')
